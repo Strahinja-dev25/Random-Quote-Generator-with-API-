fd0c0384a5279f07231709f6d712dc19ad4f92ec
Strahinja <str4l3dev16@gmail.com>
2025-08-15 22:51:38 +0200

    Added tweet and copy functions, updated CSS, and added quotes.json

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const tweetBtn = document.getElementById("tweet-quote");
 const copyBtn = document.getElementById("copy-quote");
 
 //Sve boje za pozadinu
-//const colors = [];
-//const randomColor = colors[Math.round(Math.random() * colors.length)];
-//document.body.style.backgroundColor = randomColor;
+const colors = ["#1b1b1b", "#2c3e50", "#34495e", "#2c2c54", "#1c2833", "#4b367c", "#3a3a3a", "#162447", "#222831", "#0d1b2a"];
 
 //Event Listeners
 
@@ -19,18 +17,48 @@ copyBtn.addEventListener("click", kopiraj);
 //Functions
 
 function kopiraj () {
+    const text = `"${quote.innerText}" ${author.innerText}`;
+    
+    navigator.clipboard.writeText(text).then(() => {
+        const originalText = copyBtn.innerHTML;
+        copyBtn.innerText = "Copied!";
 
+        setTimeout(() => {
+            copyBtn.innerHTML = originalText;
+        }, 2000);
+    })
+    .catch(err => {
+        console.error("Greska pri kopiranju:", err);
+    });
 }
 
 function deli () {
-
+    const text = `"${quote.innerText}" ${author.innerText}`;
+    const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(text)}`;
+    window.open(url, "_blank"); // otvara Twitter u novom tabu
 }
 
-//Treba napraviti loader
-//Error handling (try i catch)
+async function prikaz () {
 
-function prikaz () {
-    //fetch-ovanje podataka sa nekog public API-ja (npr. https://api.quotable.io/random).
-    //Prikaz tog teksta
-    //Prikaz autora
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomColor;
+    
+    try{
+        quote.innerText = "Učitavanje...";
+        author.innerText = "";
+        const response = await fetch("quotes.json");
+        if(!response.ok) throw new Error("Problem sa ucitavanjem fajla.");
+
+        const data = await response.json();
+
+        const randomIndex = Math.floor(Math.random() * data.length);
+        quote.innerText = data[randomIndex].quote;
+        author.innerText = `--${data[randomIndex].author}`;
+    }
+    catch(error){
+        quote.innerText = "Greska pri ucitavanju citata.";
+        author.innerText = "";
+        console.log(error);
+    }
 }
+
